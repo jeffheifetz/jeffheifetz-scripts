d1d572e878aebe3b11ceeeeb6360fd2d37f13dca
Jeffrey Heifetz <jheifetz@rim.com>
2012-07-09 13:20:21 -0400

    Updating setup script

--- linkFiles.sh
+++ linkFiles.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 UserName=jeff
 sudo ln -s /Users/$UserName/Coding/jeffheifetz-scripts/.bash_profile /Users/$UserName/.bash_profile
+sudo ln -s /Users/$UserName/Coding/jeffheifetz-scripts/.gitconfig /Users/$UserName/.gitconfig
 sudo ln -s /Users/$UserName/Coding/jeffheifetz-scripts/.iterm /Users/$UserName/.iterm
+sudo ln -s /Users/$UserName/Coding/jeffheifetz-scripts/.ssh/config /Users/$UserName/.ssh/config
 sudo ln -s /Users/$UserName/Coding/jeffheifetz-scripts/.vim /Users/$UserName/.vim
 sudo ln -s /Users/$UserName/Coding/jeffheifetz-scripts/.vimrc /Users/$UserName/.vimrc
-sudo ln -s /Users/$UserName/Coding/jeffheifetz-scripts/.gitconfig /Users/$UserName/.gitconfig
