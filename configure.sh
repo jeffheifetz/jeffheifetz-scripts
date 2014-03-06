871ff3c2f40772d0d22c78f9a88836129515c5e6
Chris DelCol <chris.delcol@gmail.com>
2014-03-05 23:06:32 -0500

    Remove simlinks if they already exist before creating

--- configure.sh
+++ configure.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 git submodule update --init
 Repo=$PWD
-sudo ln -s $Repo/.bash_profile $HOME/.bash_profile
-sudo ln -s $Repo/.bash_aliases $HOME/.bash_aliases
-sudo ln -s $Repo/.bashrc $HOME/.bashrc
-sudo ln -s $Repo/.gitconfig $HOME/.gitconfig
-sudo ln -s $Repo/.iterm $HOME/.iterm
-sudo ln -s $Repo/.ssh/config $HOME/.ssh/config
-sudo ln -s $Repo/.vim $HOME/.vim
-sudo ln -s $Repo/.vimrc $HOME/.vimrc
+sudo ln -fs $Repo/.bash_profile $HOME/.bash_profile
+sudo ln -fs $Repo/.bash_aliases $HOME/.bash_aliases
+sudo ln -fs $Repo/.bashrc $HOME/.bashrc
+sudo ln -fs $Repo/.gitconfig $HOME/.gitconfig
+sudo ln -fs $Repo/.iterm $HOME/.iterm
+sudo ln -fs $Repo/.ssh/config $HOME/.ssh/config
+sudo ln -fs $Repo/.vim $HOME/.vim
+sudo ln -fs $Repo/.vimrc $HOME/.vimrc
