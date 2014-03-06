36d8bf961f0c4fad5dfb90f264158b0f9064b3c0
Chris DelCol <chris.delcol@gmail.com>
2014-03-06 00:14:40 -0500

    Added ability to set git/github config settings via configure script

--- configure.sh
+++ configure.sh
@@ -9,3 +9,19 @@ sudo ln -fs $Repo/.iterm $HOME/.iterm
 sudo ln -fs $Repo/.ssh/config $HOME/.ssh/config
 sudo ln -fs $Repo/.vim $HOME/.vim
 sudo ln -fs $Repo/.vimrc $HOME/.vimrc
+
+# setup some personalized entries via question/answer
+if [ "$1" == "update" ]; then
+    echo "Enter your name used for git and press [ENTER]:"
+    read git_name
+
+    echo "Enter your email address usde for git and press [ENTER]:"
+    read git_email
+
+    echo "Enter your Github user id and press [ENTER]:"
+    read github_user
+
+    git config --global user.name "$git_name"
+    git config --global user.email "$git_email"
+    git config --global github.user "$github_user"
+fi
